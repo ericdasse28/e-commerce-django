b33f8617ae1d43958f42e7bedf411d0e5b184f74
Eric Dasse <christ.gououi@gmail.com>
2023-10-21 22:14:13 +0200

    Add Product model

---
+++ e_commerce_django/store/models/product.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+from e_commerce_django.store.models.category import Category
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=60)
+    price = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(
+        max_length=250,
+        default="",
+        blank=True,
+        null=True,
+    )
+    image = models.ImageField(upload_to="uploads/products/")
+
+    @staticmethod
+    def get_products_by_id(ids):
+        return Product.objects.filter(id__in=ids)
+
+    @staticmethod
+    def get_all_products():
+        return Product.objects.all()
+
+    @staticmethod
+    def get_all_products_by_category_id(category_id):
+        if category_id:
+            return Product.objects.filter(category=category_id)
+        else:
+            return Product.get_all_products()
