1f51371b3c2c25bbdf20b68dd190237e6fc899d5
Eric Dasse <christ.gououi@gmail.com>
2023-10-25 23:47:57 +0200

    Add orders view

---
+++ e_commerce_django/store/views/orders.py
@@ -0,0 +1,15 @@
+from django.http import HttpRequest
+from django.shortcuts import render
+from django.views import View
+from loguru import logger
+
+from e_commerce_django.store.models.order import Order
+
+
+class OrderView(View):
+    def get(self, request: HttpRequest):
+        customer = request.session.get("customer")
+        orders = Order.get_orders_by_customer(customer)
+        logger.info(orders)
+
+        return render(request, "store/orders.html", {"orders": orders})
